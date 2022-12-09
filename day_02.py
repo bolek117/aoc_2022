03d36488cd77bf4e1f09772be116a6ffb202e45c
Michal Witas <mwitas@future-processing.com>
2022-12-09 13:24:24 +0100

    Day2: part 1 done

--- day_02.py
+++ day_02.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List, Tuple
 
 from helpers import header
@@ -5,12 +6,13 @@ from helpers import header
 
 class Move:
     def __init__(self, name: str, points: int, is_opponent: bool):
-        self.op_name = name
+        self.move = name
         self.points = points
         self.is_opponent = is_opponent
 
     def __str__(self):
-        return f'{self.op_name} ({self.points} for {"me" if not self.is_opponent else "opponent"})'
+        player = 'op' if self.is_opponent else 'me'
+        return f'{player}: {self.move} ({self.points})'
 
     def __repr__(self):
         return str(self)
@@ -33,20 +35,22 @@ def map_move_sign_to_move(move_sign: str) -> Move:
 
 
 def get_strategy() -> List[Tuple[Move, Move]]:
-    with open('input_day_02.txt', 'r') as f:
+    in_file = 'input_day_02.txt' if len(sys.argv) == 1 else 'input_day_02_test.txt'
+
+    with open(in_file, 'r') as f:
         lines = f.readlines()
 
     lines = [line.strip() for line in lines]
     result = []
-    for l in lines:
-        me = map_move_sign_to_move(l[0])
-        opponent = map_move_sign_to_move(l[2])
+    for line in lines:
+        me = map_move_sign_to_move(line[0])
+        opponent = map_move_sign_to_move(line[2])
         result.append((me, opponent))
 
     return result
 
 
-def get_score_for(me: Move, opponent: Move) -> int:
+def get_score_for(op: Move, me: Move) -> int:
     r = 'rock'
     p = 'paper'
     s = 'scissors'
@@ -57,17 +61,17 @@ def get_score_for(me: Move, opponent: Move) -> int:
 
     symbol_points = -1
     result_points = -1
-    if me.op_name == r:
+    if me.move == r:
         symbol_points = 1
-        result_points = win if opponent.op_name == s else draw if opponent.op_name == r else lost
+        result_points = win if op.move == s else draw if op.move == r else lost
 
-    if me.op_name == p:
+    if me.move == p:
         symbol_points = 2
-        result_points = win if opponent.op_name == r else draw if opponent.op_name == p else lost
+        result_points = win if op.move == r else draw if op.move == p else lost
 
-    if me.op_name == s:
+    if me.move == s:
         symbol_points = 3
-        result_points = win if opponent.op_name == p else draw if opponent.op_name == s else lost
+        result_points = win if op.move == p else draw if op.move == s else lost
 
     return symbol_points + result_points
 
@@ -75,9 +79,13 @@ def get_score_for(me: Move, opponent: Move) -> int:
 def main():
     header('strategy')
     strategy = get_strategy()
+    total: int = 0
     for move in strategy:
         score = get_score_for(move[0], move[1])
         print(f'{move} = {score}')
+        total += score
+
+    print(f'Result: {total}')
 
 
 if __name__ == '__main__':
