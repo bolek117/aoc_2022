ad884f046b7aabf81868b47f8532c3b7ba2998c5
Michal Witas <mwitas@future-processing.com>
2022-12-12 09:36:17 +0100

    Day 12: gradient clusters

--- day_12.py
+++ day_12.py
@@ -12,15 +12,16 @@ class Pos:
         self.height = height
 
     def __str__(self):
-        return f'{self.x},{self.y}: {self.height}'
+        return f'{self.x}:{self.y}=({self.height})'
 
     def __repr__(self):
         return str(self)
 
 
-def load_map() -> List[Pos]:
+def load_map() -> (List[List[Pos]]):
     test_suffix = '' if len(sys.argv) == 1 else '_test'
     filename = f'input_day_12{test_suffix}.txt'
+
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -28,33 +29,83 @@ def load_map() -> List[Pos]:
     result = []
 
     x = 0
+    y = 0
     for line in lines:
         y = 0
+        row = []
         for c in line:
             if c.isupper():
                 point = Pos(x, y, 0 if c == 'S' else VAL_END)
             else:
                 point = Pos(x, y, ord(c)-ord('a')+1)
 
-            result.append(point)
+            row.append(point)
             y += 1
 
+        result.append(row)
         x += 1
 
     return result
 
 
-def find_value(map: List[Pos], value: int) -> List[Pos]:
+def find_value(map: List[List[Pos]], value: int) -> List[Pos]:
     result = []
-    for p in map:
-        if p.height == value:
-            result.append(p)
+    for row in map:
+        for p in row:
+            if p.height == value:
+                result.append(p)
 
     return result
 
 
 def get_height(character: str) -> int:
     return ord(character[0])-ord('a')+1
+
+
+class Cluster:
+    def __init__(self, cluster: List[Pos]):
+        self.e = cluster
+
+    def __str__(self):
+        r1 = self.e[0:2]
+        r2 = self.e[3:5]
+        r3 = self.e[6:8]
+
+        def row_str(row: List[Pos]):
+            return ', '.join([str(i) for i in row])
+
+        return f'{row_str(r1)}; {row_str(r2)}; {row_str(r3)}'
+
+    def __repr__(self):
+        return str(self)
+
+
+def make_clusters(map: List[List[Pos]]) -> List[Cluster]:
+    rows = len(map)
+    columns = len(map[0])
+
+
+    result = []
+    for i in range(rows):
+        for j in range(columns):
+            base = map[i][j]
+
+            def val(x, y) -> Pos:
+                if x < 0 or x >= rows or y < 0 or y >= columns:
+                    return Pos(x, y, -100)
+
+                r = map[x][y]
+                return Pos(x, y, base.height - r.height)
+
+            cluster = [
+                val(i-1, j-1), val(i-1, j), val(i-1, j+1),
+                val(i, j-1), base, val(i, j+1),
+                val(i+1, j-1), val(i+1, j), val(i+1, j+1),
+            ]
+
+            result.append(Cluster(cluster))
+
+    return result
 
 
 def main():
@@ -64,6 +115,8 @@ def main():
 
     pos_z = find_value(map, get_height('z'))
     pos_a = find_value(map, get_height('a'))
+
+    clusters = make_clusters(map)
     pass
 
 
